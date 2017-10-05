894d6b846f17bfe6909bf79aaeba761945d2b408
Shlomi Oliel <ulielshlomi@gmail.com>
2017-10-05 10:20:44 +0100

    added styles

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -10,7 +10,6 @@
 import ready from 'lite-ready'; // https://github.com/nicbell/liteready
 import svg4everybody from 'svg4everybody'; // https://github.com/jonathantneal/svg4everybody
 import axios from 'axios'
-import async from 'async'
 // Global libs that don't return a value
 import 'console';
 import 'kickoff-welcome.js'; // The Kickoff message in the js console. Remove it if you like :)
@@ -24,74 +23,37 @@ ready(() => {
         polyfill: true, // polyfill <use> elements for External Content
     });
 
+		function html_unescape(s) {
+				var div = document.createElement("div");
+				div.innerHTML = s;
+				return div.textContent || div.innerText; 
+		}
 
+		const USD = document.getElementById("bit-usd")
+		const GBP = document.getElementById("bit-gbp")
 
-    let app = { "title": "TMW - Test" };
+		USD.innerHTML = "Loading..."
+		GBP.innerHTML = "Loading..."
 
-    (function(app) {
+		const getBitcoinUSD = axios({
+				method: 'get',
+				url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
+				withCredentials: false
+		}).then(response => response.data);
 
-        function html_unescape(s) {
-            var div = document.createElement("div");
-            div.innerHTML = s;
-            return div.textContent || div.innerText; // IE is different
-        }
+		const convertToGBP = axios({
+				method: 'get',
+				url: 'https://openexchangerates.org/api/latest.json?app_id=242ea76d0c784a6abb12ca429467b288',
+				withCredentials: false
+		}).then(response => response.data);
+		
+		Promise.all([getBitcoinUSD, convertToGBP]).then(([resultFromUSD, resultFromGBP]) => {
+			USD.innerHTML = `${html_unescape(resultFromUSD.bpi.USD.symbol)}${resultFromUSD.bpi.USD.rate}`;
+			GBP.innerHTML = `${html_unescape("&pound") } ${(resultFromUSD.bpi.USD.rate_float*resultFromGBP.rates.GBP).toFixed(4) }`
+		})
+		.catch(error => {
+			USD.innerHTML = "Error";
+			GBP.innerHTML = "Error";
+		})
 
-        const USD = document.getElementById("bit-usd")
-        const GBP = document.getElementById("bit-gbp")
-
-        USD.innerHTML = "Loading..."
-        GBP.innerHTML = "Loading..."
-
-
-        const getBitcoinUSD = function(callback) {
-
-            axios({
-								method: 'get',
-								url: 'https://api.coindesk.com/v1/bpi/currentprice.json',
-								withCredentials: false
-						})
-						.then(function(response) {
-
-								callback(null, response.data)
-						})
-						.catch(function(error) {
-								callback("Error getting Bitcoin/USD Rate");
-						});
-
-        }
-
-        const convertToGBP = function(callback) {
-
-					axios({
-							method: 'get',
-							url: 'https://openexchangerates.org/api/latest.json?app_id=242ea76d0c784a6abb12ca429467b288',
-							withCredentials: false
-					})
-					.then(function(response) {
-
-							callback(null, response.data)
-					})
-					.catch(function(error) {
-							console.log(error)
-							callback("Error getting USD/GBP Rate");
-					});
-        }
-
-        async.parallel([
-						getBitcoinUSD,
-						convertToGBP
-				],
-				function(err, results) {
-
-						if (err) {
-								GBP.innerHTML = err
-								USD.innerHTML = err
-						} else {
-								USD.innerHTML = `${html_unescape(results[0].bpi.USD.symbol)}${results[0].bpi.USD.rate}`;
-								GBP.innerHTML = `${html_unescape("&pound") } ${(results[0].bpi.USD.rate_float*results[1].rates.GBP).toFixed(4) }`
-						}
-				});
-
-    })(window)
-    // moduleTest(); // this is a test, uncomment this line to try it
 });
